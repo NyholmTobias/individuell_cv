35c90f630a52d23f619d32da9596426fb561d2f9
NyholmTobias <nyholm.tobias@gmail.com>
2021-06-01 14:20:20 +0200

    spelling fixes

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -17,7 +17,7 @@ const AboutMe = () => {
     return (
         <div className="col-xl-7 col-l-7 col-md-8 col-sm-8 col-xs-8 float-right">
             <h2>About Me</h2>
-            <p>In my life I've been working with everything from beeing a warehouse worker, tire fitter, sound technician, customer service and grapichal purchasing. Through the years i have gathered a broad understanding of different businesses and found out what makes me shine and keeps me motivated. I love to work with projects and to learn new skills while I always try to find ways to optimise my what i do. With this knowlege i have now started to aim towards working with a longtime interest and hobby of mine. Software- and system development.</p>
+            <p>In my life I've been working with everything from beeing a warehouse worker, tire fitter, sound technician, customer service employee and a grapichal purchaser. Through the years i have gathered a broad understanding of different businesses and found out what really makes me shine and keeps me motivated. I love to work with projects and to learn new skills while at the same time try to find ways to optimise what i do. With this knowlege i have now started to aim towards working with a longtime interest and hobby of mine. Software- and system development.</p>
             <ul className="col-xl-6 col-l-6 col-md-12 col-sm-12 col-xs-12 float-left">
                 <li><p><b>Born:</b> August 23 1993</p></li>
                 <li><p><b>Lives:</b> Gothenburg, Sweden</p></li>
