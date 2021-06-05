d43980c43eb4c0684629a9b4b64a92d6a53d8b47
NyholmTobias <nyholm.tobias@gmail.com>
2021-06-05 14:36:01 +0200

    fixed some spelling

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -17,7 +17,7 @@ const AboutMe = () => {
     return (
         <div className="col-xl-7 col-l-7 col-md-8 col-sm-8 col-xs-8 float-right">
             <h2>About Me</h2>
-            <p>In my life I've been working with everything from beeing a warehouse worker, tire fitter, sound technician, customer service employee and a grapichal purchaser. Through the years i have gathered a broad understanding of different businesses and found out what really makes me shine and keeps me motivated. I love to work with projects and to learn new skills while at the same time try to find ways to optimise what i do. With this knowlege i have now started to aim towards working with a longtime interest and hobby of mine. Software- and system development.</p>
+            <p>In my life I've been working with everything from beeing a warehouse worker, tire fitter, sound technician, customer service employee and a graphical purchaser. Through the years I have gathered a broad understanding of different businesses and found out what really makes me shine and keeps me motivated. I love to work with projects and to learn new skills while at the same time try to find ways to optimise what I do. With this knowlege I have now started to aim towards working with a longtime interest and hobby of mine. Software- and system development.</p>
             <ul className="col-xl-6 col-l-6 col-md-12 col-sm-12 col-xs-12 float-left">
                 <li><p><b>Born:</b> August 23 1993</p></li>
                 <li><p><b>Lives:</b> Gothenburg, Sweden</p></li>
@@ -26,7 +26,7 @@ const AboutMe = () => {
             </ul>
             <ul className="col-xl-6 col-l-6 col-md-12 col-sm-12 col-xs-12 float-left">
                 <li><p><b>Interests:</b> Computers, games, music, cooking, training, diving.</p></li>
-                <li><p><b>Life Goals:</b> House by the sea. Work abroad. Run my own company. Get a dog when i retire.</p></li>
+                <li><p><b>Life Goals:</b> House by the sea. Work abroad. Lead projects. Get a dog when I retire.</p></li>
                 <li><p><b>Part Time Occupation:</b><br/> Junior developer at Centiro. Graphical purchaser at Tingstad.</p></li>
             </ul>
         </div>
--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -4,7 +4,7 @@ import "./Timeline.css"
 const Timeline = () => {
     return(
         <div className="text-center">
-            <h2 className="mb-5">Timeline of academic and professional achievements</h2>
+            <h2 className="mb-5">Timeline of Academic and Professional Achievements</h2>
             <div className="timeline">
                 <div className="containerTL left">
                     <div className="contentTL">
@@ -17,14 +17,14 @@ const Timeline = () => {
                     <div className="contentTL">
                         <h2>2009(Fall)-2012(Spring)</h2>
                         <h4 className="text-left">Magnus- Åbergsgymnasiet - Music program</h4>
-                        <p className="text-left">During my time in the gymnasium i studied music, with drums as my main instrument.</p>
+                        <p className="text-left">During my time in the gymnasium I studied music, with drums as my main instrument.</p>
                     </div>
                 </div>
                 <div className="containerTL left">
                     <div className="contentTL">
                         <h2>2012(Summer)-2015(spring)</h2>
                         <h4 className="text-left">The "Way Finder Era"</h4>
-                        <p className="text-left">In this time of my life had a bunch of jobs trying to find out what i want to do with life. I was working in four different warehouses, was swaping tires in a tire workshop, and studied music production for six months while working as a soundtechnician on the weekends.</p>
+                        <p className="text-left">In this time of my life had a bunch of jobs trying to find out what I want to do with life. I was working in four different warehouses, was swaping tires in a tire workshop, and studied music production for six months while working as a soundtechnician on the weekends.</p>
                     </div>
                 </div>
                 <div className="containerTL right">
@@ -38,21 +38,21 @@ const Timeline = () => {
                     <div className="contentTL">
                         <h2>2016(December)-2017(May)</h2>
                         <h4 className="text-left">Tingstad Papper AB - Customer Service</h4>
-                        <p className="text-left">While i worked in the warehouse a spot became open at the customer service, I applied for it and got it! Customer service made me learn how to handle customers in a efficient way by using the knowlege i learnt in the warehouse, but also it helped me understand how the workflow is constributed in a major company. </p>
+                        <p className="text-left">While I worked in the warehouse a spot became open at the customer service, I applied for it and got it! Customer service made me learn how to handle customers in a efficient way by using the knowlege I learnt in the warehouse, but also it helped me understand how the workflow is constributed in a major company. </p>
                     </div>
                 </div>
                 <div className="containerTL right">
                     <div className="contentTL">
                         <h2>2017(May)-Present</h2>
                         <h4 className="text-left">Tingstad Papper AB - Graphical purchaser</h4>
-                        <p className="text-left">After working with customer service for half a year i was asked to start working at the graphical department as a purchaser. My main task is to purchase printed items from an array of over 500 suppliers located all over the world and make sure that the orders come to us at the set date, with the right quantities and for the right price. This includes doing both strategical and operational purchasing, writing offers, ask for offers and to monitor deliveries. Apart from my regular assignments i got the chance to work with several differnt projects mostly to improve the workflow of the graphical department but also to help calculate and moderate the complaint costs of the whole company. I still work there on holidays or when there is less to do in school.</p>
+                        <p className="text-left">After working with customer service for half a year I was asked to start working at the graphical department as a purchaser. My main task is to purchase printed items from an array of over 500 suppliers located all over the world and make sure that the orders come to us at the set date, with the right quantities and for the right price. This includes doing both strategical and operational purchasing, writing offers, asking for offers and to monitor deliveries. Apart from my regular assignments I got the chance to work with several differnt projects mostly to improve the workflow of the graphical department but also to help calculate and moderate the complaint costs of the whole company. I still work there on holidays or when there is less to do in school.</p>
                     </div>
                 </div>
                 <div className="containerTL left">
                     <div className="contentTL">
                         <h2>2020-2022(June)</h2>
                         <h4 className="text-left">NBI-/Handelsakademin - System Development, Object Oriented Programing</h4>
-                        <p className="text-left">Altough I learnt alot at Tingstad and got the chance to try many differnt things, I eventually felt that I hit a plateau and I didnt want to work with those kind of products anymore. I wanted to work more with projects and to get more logically challenging assignments. So i decided to chase one of my life time interests and started to study System Development. It's a broad education that touches most of the aspects of System Development. The main language has been C#, but we have gone over more (see skills section).</p>
+                        <p className="text-left">Altough I learnt alot at Tingstad and got the chance to try many differnt things, I eventually felt that I hit a plateau and I didnt want to work with those kind of products anymore. I wanted to work more with projects and to get more logically challenging assignments. So I decided to chase one of my life time interests and started to study System Development. It's a broad education that touches most of the aspects of System Development. The main language has been C#, but we have gone over more (see skills section).</p>
                     </div>
                 </div>
                 <div className="containerTL right">
